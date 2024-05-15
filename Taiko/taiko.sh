ec065539bde1518d4adebad586d7ee10b7fba533
Hananonamida <Hananonamida@outlook.com>
2024-05-15 16:18:32 +0800

    taiko

--- Taiko/taiko.sh
+++ Taiko/taiko.sh
@@ -10,6 +10,7 @@ fi
 # 获取传入的参数
 l1_proposer_private_key=$1
 l2_suggested_fee_recipient=$2
+l1_endpoint=$3
 
 #安装Taiko节点------------------------------
 # 更新系统包列表
@@ -37,8 +38,8 @@ if [ ! -f .env ]; then
 fi
 
 # 提示用户输入环境变量的值
-l1_endpoint_http="http://138.201.221.84:8545"
-l1_endpoint_ws="ws://138.201.221.84:8546"
+l1_endpoint_http="http://$l1_endpoint:8545"
+l1_endpoint_ws="ws://$l1_endpoint:8546"
 l1_beacon_http="http://unstable.holesky.beacon-api.nimbus.team"
 prover_endpoints="http://198.244.201.79:9876,https://prover-hekla.taiko.tools,https://prover2-hekla.taiko.tools,http://taiko-a7-prover.zkpool.io,http://146.59.55.26:9876,http://kenz-prover.hekla.kzvn.xyz:9876,http://hekla.stonemac65.xyz:9876,http://51.91.70.42:9876,http://taiko.web3crypt.net:9876,http://148.113.17.127:9876,http://hekla.prover.taiko.coinblitz.pro:9876,http://taiko-testnet.m51nodes.xyz:9876,http://148.113.16.26:9876,http://51.161.118.103:9876,http://162.19.98.173:9876,http://49.13.215.95:9876,http://49.13.143.184:9876,http://49.13.210.192:9876,http://159.69.242.22:9876,http://49.13.69.238:9876,http://taiko.guru:9876,http://taiko.donkamote.xyz:9876"
 enable_proposer="true"
